d060cd8e5c4cdd5183bec99d9f2d563cf8b75b37
AkshitTupkar <2024bcs156@sggs.ac.in>
2025-08-05 12:56:00 +0530

    stats-animation

--- src/components/whoarewe.jsx
+++ src/components/whoarewe.jsx
@@ -1,4 +1,5 @@
 import "./whoarewe.css"
+import CountUp from 'react-countup';  // npm i react-countup
 
 const Whoarewe = () => {
   return (
@@ -7,7 +8,7 @@ const Whoarewe = () => {
          <div className="wrapper">
          <div className="left">
             <div className="heading">
-            <h1>Who Are We?</h1>
+            <h1><b>Who Are We?</b></h1>
          </div>
             <div className="content">
             <p>We're a bunch of young people trying to solve as many problems as possible for our community and giving them an optimum, all-in-one platform for their overall growth.</p>
@@ -19,20 +20,20 @@ const Whoarewe = () => {
          </div>
 
        <div className="stats-container">
-            <div className="stat-box">
-              <div className="stat-number">2012</div>
+            <div className="stat-box"> 
+              <div className="stat-number"><CountUp start={2026} end={2012} duration={5}/></div>
               <div className="stat-label">Since</div>
             </div>
             <div className="stat-box">
-               <div className="stat-number">100+</div>
+               <div className="stat-number"><CountUp end={100} duration={3} />+</div>
                <div className="stat-label">Current Members</div>
             </div>
             <div className="stat-box">
-               <div className="stat-number">32+</div>
+               <div className="stat-number"><CountUp end={32} duration={4}/>+</div>
                <div className="stat-label">Research Papers</div>
             </div>
             <div className="stat-box">
-               <div className="stat-number">5+</div>
+               <div className="stat-number"><CountUp end={5} duration={4} />+</div>
                <div className="stat-label">Patents Filed</div>
             </div>
        </div>
